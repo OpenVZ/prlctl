e54784e1389f1a4e0a15f503399fa2fd5e085400
Dmitry Mishin <dim@virtuozzo.com>
2016-03-31 12:39:35 +0300

    Polish the utility output in 'exec' cancellation case
    When a guest command is cancelled, we do not wait (and don't care)
    when it is finished. Thus we have a situation when the job ret code
    is zero and there is no parameter with ret code of the executed
    command. This patch handles this case and removes wrong error output
    on cancellation.
    
    PSBM-45482

--- src/PrlVm.cpp
+++ src/PrlVm.cpp
@@ -954,25 +954,34 @@ int PrlVm::exec(const CmdParamData &param)
 			prl_err(ret, "PrlJob_GetResult: %s",  get_error_str(ret).c_str());
 			break;
 		}
-		PrlHandle hEvent;
-		if ((ret = PrlResult_GetParamByIndex(hResult.get_handle(), 0, hEvent.get_ptr()))) {
-			prl_err(ret, "PrlResult_GetParamByIndex: %s",  get_error_str(ret).c_str());
+
+		PRL_UINT32 nCount = 0;
+		if ((ret = PrlResult_GetParamsCount(hResult.get_handle(), &nCount))) {
+			prl_err(ret, "PrlResult_GetParamsCount: %s", get_error_str(ret).c_str());
 			break;
 		}
 
-		PrlHandle hRetCode;
-		if ((ret = PrlEvent_GetParamByName(hEvent.get_handle(), EVT_PARAM_VM_EXEC_APP_RET_CODE, hRetCode.get_ptr()))) {
-			prl_err(ret, "PrlEvent_GetParamByName (EVT_PARAM_VM_EXEC_APP_RET_CODE): %s",  get_error_str(ret).c_str());
-			break;
-		}
+		if (nCount > 0) {
+			PrlHandle hEvent;
+			if ((ret = PrlResult_GetParamByIndex(hResult.get_handle(), 0, hEvent.get_ptr()))) {
+				prl_err(ret, "PrlResult_GetParamByIndex: %s",  get_error_str(ret).c_str());
+				break;
+			}
 
-		PRL_UINT32 retcode;
-		if ((ret = PrlEvtPrm_ToUint32(hRetCode.get_handle(), &retcode))) {
-			prl_err(ret, "PrlEvtPrm_ToUint32: %s",  get_error_str(ret).c_str());
-			break;
-		}
+			PrlHandle hRetCode;
+			if ((ret = PrlEvent_GetParamByName(hEvent.get_handle(), EVT_PARAM_VM_EXEC_APP_RET_CODE, hRetCode.get_ptr()))) {
+				prl_err(ret, "PrlEvent_GetParamByName (EVT_PARAM_VM_EXEC_APP_RET_CODE): %s",  get_error_str(ret).c_str());
+				break;
+			}
 
-		ret = retcode;
+			PRL_UINT32 retcode;
+			if ((ret = PrlEvtPrm_ToUint32(hRetCode.get_handle(), &retcode))) {
+				prl_err(ret, "PrlEvtPrm_ToUint32: %s",  get_error_str(ret).c_str());
+				break;
+			}
+
+			ret = retcode;
+		}
 	} while (0);
 
 #ifdef _LIN_
