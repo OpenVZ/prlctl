OpenVZ/prlctl

commit 7a94a73542538b75fb1f0504f837a40b51c997f5

Support encrypt, decrypt and change key actions for HDDs (PSBM-52088)
Signed-off-by: Igor Bazhitov <ibazhitov@virtuozzo.com>

src/PrlDev.cpp
@@ -620,6 +620,38 @@ int PrlDevHdd::set_serial_number(const std::string &serial)
 	return 0;
 }
 
+PRL_RESULT PrlDevHdd::apply_encryption(const DevInfo &param)
+{
+	std::string old_keyid = get_encryption_keyid();
+	std::string keyid = param.enc_keyid;
+
+	if (param.cmd == Set) {
+		switch (param.enc_action) {
+		case ENC_SET:
+			if (old_keyid.empty())
+				return prl_err(PRL_ERR_INVALID_ARG, "The disk is not encrypted. "
+					"To encrypt the disk, specify the --encrypt option.");
+			break;
+
+		case ENC_ENCRYPT:
+			if (!old_keyid.empty())
+				return prl_err(PRL_ERR_INVALID_ARG, "The disk is already encrypted.");
+			break;
+
+		case ENC_DECRYPT:
+			if (old_keyid.empty())
+				return prl_err(PRL_ERR_INVALID_ARG, "The disk is not encrypted.");
+			keyid.clear();
+			break;
+
+		default:
+			return PRL_ERR_INVALID_ARG;
+		}
+	}
+
+	return set_encryption_keyid(keyid);
+}
+
 int PrlDevHdd::set_device(const DevInfo &param)
 {
 	PRL_RESULT ret;
@@ -669,6 +701,8 @@ int PrlDevHdd::set_device(const DevInfo &param)
 		if ((ret = resize_image(param)))
 			return ret;
 		return 0;
+	} else if (param.enc_action != ENC_NONE) {
+		return apply_encryption(param);
 	} else {
 		/* Skip to create new image on the set action */
 		if (param.cmd == Set)

bash_completion/prlctl.sh
@@ -87,7 +87,8 @@ _prlctl()
 		--autostart --autstart-delay --autostop \
 		--vnc-mode --vnc-port --vnc-passwd --ioprio \
 		--ha-enable --ha-prio'
-	local set_device_hdd_flags='--image --type --size --split --iface --position --device --passthr'
+	local set_device_hdd_flags='--image --type --size --split --iface --position --device --passthr \
+		--encrypt --decrypt --encryption-keyid --reencrypt --nowipe'
 	local set_device_cdrom_flags='--device --image --iface --position --passthr'
 	local set_device_net_flags='--type --mac --iface'
 	local set_device_fdd_flags='--device --image --recreate'

man/prlctl.8
@@ -522,6 +522,30 @@ If the \fB--no-fs-resize\fR option is specified, the last partition on the hard 
 .br
 \fBmnt\fR: the mount point to automount virtual hard disk inside the guest OS
 
+.br
+\fBManaging virtual hard disk drives encryption\fR
+.br
+
+\fB--device-add\fR \fIhdd\fR \fB--encryption-keyid\fR <\fIkey_id\fR> [\fBother HDD options\fR]
+.br
+\fB--device-set\fR \fIhdd\fR \fB--encrypt\fR \fB--encryption-keyid\fR <\fIkey_id\fR> [\fB--nowipe\fR]
+.br
+\fB--device-set\fR \fIhdd\fR \fB--decrypt\fR [\fB--nowipe\fR]
+.br
+\fB--device-set\fR \fIhdd\fR \fB--encryption-keyid\fR <\fIkey_id\fR> [\fB--nowipe\fR] [\fB--reencrypt\fR]
+
+.br
+\fBencrypt\fR: encrypt the hard disk drive contents with an encryption key given in the \fB--encryption-keyid\fR option
+.br
+\fBdecrypt\fR: decrypt the encrypted hard disk drive
+.br
+\fBencryption-keyid\fR: the identifier of a key used to encrypt the hard disk drive. When this option is used along with the \fB--encrypt\fR option, the provided key identifier is used to encrypt the non-encrypted virtual disk drive. When this option is used with the \fB--device-set\fR command, the already existing encryption key is changed to the provided one.
+.br
+\fBreencrypt\fR: re-encrypt the entire disk contents when changing the encryption key identifier. When the \fB--device-set\fR command is called with the \fB--encryption-keyid\fR option, only a small amount of encrypted data (i.e. LUKS header) is changed in the virtual disk drive. When the \fB--reencrypt\fR option is added to the command above, the entire disk contents are re-encrypted.
+.br
+\fBnowipe\fR: remove the original virtual hard disk after it had been encrypted without securely erasing its data from the host disk drive. Without this option, after a virtual disk had been successfully encrypted, the original unencrypted data is securely erased from the host hard disk drive.
+.br
+
 \fBConnecting physical hard disks to VE\fR
 .br
 \fB--device-add\fR \fIhdd\fR \fB--device\fR <\fIname\fR> [\fB--iface\fR <\fIide\fR|\fIscsi\fR|\fIvirtio\fR>] [\fB--position\fR <\fIn\fR>]

src/CmdParam.cpp
@@ -189,8 +189,12 @@ static Option set_options[] = {
 	{"disable", '\0', OptNoArg, CMD_DISABLE},
 	{"connect", 'c', OptNoArg, CMD_CONNECT},
 	{"disconnect", '\0', OptNoArg, CMD_DISCONNECT},
-	{"encryption-keyid", '\0', OptRequireArg, CMD_ENC_KEYID},
 	{"serial", '\0', OptRequireArg, CMD_SERIAL_NUMBER},
+	{"encryption-keyid", '\0', OptRequireArg, CMD_ENC_KEYID},
+	{"encrypt", '\0', OptNoArg, CMD_ENC_ENCRYPT},
+	{"decrypt", '\0', OptNoArg, CMD_ENC_DECRYPT},
+	{"reencrypt", '\0', OptNoArg, CMD_ENC_REENCRYPT},
+	{"no-wipe", '\0', OptNoArg, CMD_ENC_NOWIPE},
 
 	{"hdd-block-size", '\0', OptRequireArg, CMD_HDD_BLOCK_SIZE},
 
@@ -1872,6 +1876,23 @@ bool CmdParamData::is_valid()
 		return false;
 	}
 
+	if (dev.cmd != Set && dev.enc_flags != 0) {
+		fprintf(stderr, "Disk encryption options can only be used with "
+			"the --device-set command.\n");
+		return false;
+	}
+
+	if (dev.enc_flags != 0 && dev.enc_action == ENC_NONE) {
+		fprintf(stderr, "Disk encryption options --reencrypt and --no-wipe can only be used with "
+			"the --encrypt, --decrypt or --encryption-keyid commands.\n");
+	}
+
+	if (dev.enc_action == ENC_ENCRYPT && dev.enc_keyid.empty()) {
+		fprintf(stderr, "The --encryption-keyid option must be specified "
+			"with the --encrypt option.\n");
+		return false;
+	}
+
 	return true;
 }
 
@@ -2165,6 +2186,22 @@ CmdParamData cmdParam::get_param(int argc, char **argv, Action action,
 			break;
 		case CMD_ENC_KEYID:
 			param.dev.enc_keyid = val;
+			break;
+		case CMD_ENC_ENCRYPT:
+		case CMD_ENC_DECRYPT:
+			if (param.dev.enc_action != ENC_NONE) {
+				fprintf(stderr, "Only one of the options, --encrypt "
+					"or --decrypt, can be specified per command.\n");
+				return invalid_action;
+			}
+			param.dev.enc_action = (id == CMD_ENC_ENCRYPT)
+				? ENC_ENCRYPT : ENC_DECRYPT;
+			break;
+		case CMD_ENC_REENCRYPT:
+			param.dev.enc_flags |= PCEF_CHANGE_MASTER_KEY;
+			break;
+		case CMD_ENC_NOWIPE:
+			param.dev.enc_flags |= PCEF_DONT_SHRED_PLAIN_DATA;
 			break;
 		case CMD_DEVICE_DEL:
 #ifdef _LIN_
@@ -3271,6 +3308,10 @@ CmdParamData cmdParam::get_param(int argc, char **argv, Action action,
 		param.dev.type = DEV_HDD;
 		param.dev.cmd = Set;
 	}
+
+	if (!param.dev.enc_keyid.empty() && param.dev.enc_action == ENC_NONE)
+		param.dev.enc_action = ENC_SET;
+
 	if (!param.set_net_param(net))
 		 return invalid_action;
 	if (!param.is_valid())

src/CmdParam.h
@@ -219,6 +219,13 @@ enum {
 	AM_VM		= PGS_CONNECTED_TO_VM,
 };
 
+enum EncryptionAction {
+	ENC_NONE,
+	ENC_SET,
+	ENC_ENCRYPT,
+	ENC_DECRYPT,
+};
+
 class DevInfo {
 public:
 	Command cmd;
@@ -256,8 +263,10 @@ public:
 	int autocompact;
 	bool offline;
 	std::string storage_url;
-	std::string enc_keyid;
 	std::string serial_number;
+	std::string enc_keyid;
+	unsigned int enc_flags;
+	EncryptionAction enc_action;
 
 public:
 	DevInfo() :
@@ -279,7 +288,9 @@ public:
 		hdd_block_size(0),
 		no_fs_resize(0),
 		autocompact(-1),
-		offline(false)
+		offline(false),
+		enc_flags(0),
+		enc_action(ENC_NONE)
 	{}
 
 	bool is_updated() const
@@ -1438,8 +1449,13 @@ enum cmdOptions {
 	CMD_EXEC_NO_SHELL,
 
 	CMD_NO_TUNNEL,
-	CMD_ENC_KEYID,
 	CMD_SERIAL_NUMBER,
+
+	CMD_ENC_KEYID,
+	CMD_ENC_ENCRYPT,
+	CMD_ENC_DECRYPT,
+	CMD_ENC_REENCRYPT,
+	CMD_ENC_NOWIPE,
 };
 
 #endif // __CMDPARAM_H__

src/PrlDev.h
@@ -148,6 +148,7 @@ private:
 	int create_image(const DevInfo &param);
 	int resize_image(const DevInfo &param);
 	int set_device(const DevInfo &param);
+	PRL_RESULT apply_encryption(const DevInfo &param);
 };
 
 class PrlDevCdrom : public PrlDev {

src/PrlVm.cpp
@@ -3904,7 +3904,12 @@ int PrlVm::set(const CmdParamData &param)
 	}
 
 	if (is_updated()) {
-		if ((ret = commit_configuration(param)))
+		if (param.dev.cmd == Set && param.dev.enc_action != ENC_NONE) {
+			ret = commit_encryption(param);
+		} else {
+			ret = commit_configuration(param);
+		}
+		if (ret)
 			return ret;
 
 		load_config();
@@ -4888,3 +4893,12 @@ int PrlVm::detach_backup_disks(const std::string& id)
 	return 0;
 }
 
+int PrlVm::commit_encryption(const CmdParamData &param)
+{
+	std::string err;
+	PrlHandle job(PrlVm_CommitEncryption(m_hVm, param.dev.enc_flags, 0, 0));
+	PRL_RESULT ret = get_job_retcode(job.get_handle(), err);
+	if (ret)
+		prl_err(ret, "PrlVm_CommitEncryption failed: %s", err.c_str());
+	return ret;
+}

src/PrlVm.h
@@ -271,6 +271,7 @@ private:
 	int attach_backup_disks(const CmdParamData& param);
 	int detach_backup_disks(const std::string& id);
 	void search_attached_backups(std::list<PrlDevHdd *>& disks);
+	int commit_encryption(const CmdParamData &param);
 };
 
 #endif // __PRLVM_H__

commit 86994ef0edb9bdb82142387569567afbe90a8cbd

Default disk size used if encrypted image created #PSBM-53859
Create image logic was broken by commit 7a94a735.

Signed-off-by: Igor Sukhih <igor@virtuozzo.com>

src/PrlDev.cpp
@@ -701,12 +701,13 @@ int PrlDevHdd::set_device(const DevInfo &param)
 		if ((ret = resize_image(param)))
 			return ret;
 		return 0;
-	} else if (param.enc_action != ENC_NONE) {
-		return apply_encryption(param);
 	} else {
 		/* Skip to create new image on the set action */
-		if (param.cmd == Set)
+		if (param.cmd == Set) {
+			 if (param.enc_action != ENC_NONE)
+				return apply_encryption(param);
 			return 0;
+		}
 
 		type = PDT_USE_IMAGE_FILE;
 		/* generate hdd image name */