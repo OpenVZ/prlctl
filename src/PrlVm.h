610df5544eb15206e55c4f007521da38e1681e87
Konstantin Vlasov <kvlasov@virtuozzo.com>
2017-02-13 15:32:45 +0300

    Reveal guest tools management to user
    * 'installtools' command added to the usage info and manual.
    * Guest tools autoupdate flag is displayed in 'prlctl list -i'.

--- src/PrlVm.h
+++ src/PrlVm.h
@@ -168,6 +168,7 @@ public:
 	int set_memguarantee(const CmdParamData &param);
 	int set_desc(const std::string &desc);
 	std::string get_desc() const;
+	bool get_autoupdategt() const;
 	int set_autostart(const std::string &mode);
 	std::string get_autostart_info() const;
 	int set_autostart_delay(unsigned int delay);
