f08398fcc2c6ac9fde7ec74b5aa1c8d95fbd3688
Konstantin Vlasov <kvlasov@virtuozzo.com>
2017-02-13 16:03:11 +0300

    Sync method name with SDK

--- src/PrlVm.h
+++ src/PrlVm.h
@@ -168,7 +168,7 @@ public:
 	int set_memguarantee(const CmdParamData &param);
 	int set_desc(const std::string &desc);
 	std::string get_desc() const;
-	bool get_autoupdategt() const;
+	bool is_tools_auto_update_enabled() const;
 	int set_autostart(const std::string &mode);
 	std::string get_autostart_info() const;
 	int set_autostart_delay(unsigned int delay);
