OpenVZ/prlctl

commit eb814b19090c1bab0f8fe1e0dbd55679f9e1cf3b

Do not request VM/CT list on create
It used to check name confilct, but it checked in Dispatcher.

Signed-off-by: Igor Sukhih <igor@virtuozzo.com>

src/PrlSrv.cpp
@@ -705,12 +705,6 @@ int PrlSrv::create_ct(const CmdParamData &param)
 	PRL_UINT32 resultCount;
 	PrlVm *vm;
 
-	if ((ret = update_vm_list(PVTF_CT|PVTF_VM)))
-		return ret;
-
-	if (m_VmList.find(param.id))
-		return prl_err(-1, "The %s VM already exists.",
-			param.id.c_str());
 	prl_log(0, "Creating the Container...");
 
 	PrlHandle hVm;
@@ -763,12 +757,6 @@ int PrlSrv::create_vm(const CmdParamData &param)
 	PRL_RESULT ret;
 	PrlVm *vm;
 
-	if ((ret = update_vm_list(PVTF_CT|PVTF_VM)))
-		return ret;
-
-	if (m_VmList.find(param.id))
-		return prl_err(-1, "The %s VM already exists.",
-			param.id.c_str());
 	/* in case ostemplate specified just clone VM */
 	if (!param.ostemplate.empty()) {
 		prl_log(0, "Creating the VM on the basis of the %s template...",

commit 86920e3a76c3e8f22a4d3071d97b07ba1bf1b45b

Failed to create CT from template #PSBM-70392
Get all Vm list (broken by commit eb814b19090c "Do not request VM/CT list on create")

Signed-off-by: Igor Sukhih <igor@virtuozzo.com>

src/PrlSrv.cpp
@@ -761,6 +761,9 @@ int PrlSrv::create_vm(const CmdParamData &param)
 	if (!param.ostemplate.empty()) {
 		prl_log(0, "Creating the VM on the basis of the %s template...",
 			param.ostemplate.c_str());
+		if ((ret = update_vm_list(PVTF_CT|PVTF_VM)))
+			return ret;
+
 		if (!(vm = m_VmList.find(param.ostemplate)) ||
 		    !vm->is_template())
 			return prl_err(1, "Failed to find the %s template.",