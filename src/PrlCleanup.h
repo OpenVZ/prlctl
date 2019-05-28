87b56067ac605112259d854a4454dd3f5af5e187
Igor Sukhih <igor@virtuozzo.com>
2019-05-28 16:33:39 +0300

    Update Copyright
    Copyright (c) 2017-2019 Virtuozzo International GmbH. All rights reserved.
    
    Signed-off-by: Igor Sukhih <igor@virtuozzo.com>

--- src/PrlCleanup.h
+++ src/PrlCleanup.h
@@ -8,6 +8,7 @@
  * @author igor@
  *
  * Copyright (c) 2005-2017, Parallels International GmbH
+ * Copyright (c) 2017-2019 Virtuozzo International GmbH. All rights reserved.
  *
  * This file is part of OpenVZ. OpenVZ is free software; you can redistribute
  * it and/or modify it under the terms of the GNU General Public License as
@@ -24,7 +25,7 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
  * 02110-1301, USA.
  *
- * Our contact details: Parallels International GmbH, Vordergasse 59, 8200
+ * Our contact details: Virtuozzo International GmbH, Vordergasse 59, 8200
  * Schaffhausen, Switzerland.
  */
 
