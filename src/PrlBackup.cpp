6949dbe5aa227d3d1a71afae44c98a55f439a5f9
Igor Sukhih <igor@virtuozzo.com>
2020-06-03 15:51:01 +0300

    Test PrlVm_BeginBackup functionality #PSBM-104586
     prlctl backup ID --abackup-mode -f --backup-path <path>
     prlctl backup ID --abackup-mode -i --backup-path <path> --uuid <UUID>
    
    After backup will completed 3 files will be created
     $path/$diskN.full
     $path/$diskN.delta
     $path/$diskN.cbt
    
    full - all block from snapshot
    delta - block from CBT bitmap
    cbt - cbt bitmap
    
    Signed-off-by: Igor Sukhih <igor@virtuozzo.com>

--- src/PrlBackup.cpp
+++ src/PrlBackup.cpp
@@ -25,6 +25,13 @@
 #include <string.h>
 #include <stdio.h>
 #include <memory>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <errno.h>
+
+#include <iostream>
+#include <fstream>
 
 #include <boost/property_tree/xml_parser.hpp>
 #include <boost/foreach.hpp>
@@ -208,15 +215,227 @@ int PrlSrv::do_vm_backup(const PrlVm& vm, const CmdParamData &param,
 	return ret;
 }
 
+struct cbt_bitmap
+{
+	typedef unsigned long bmap_t;
+	cbt_bitmap(unsigned long size, unsigned int bsize) :
+			bsize(size), gran(bsize), map(NULL)
+	{}
+	~cbt_bitmap()
+	{
+		release();
+	}
+
+	bmap_t *alloc()
+	{
+		bits = bsize / gran;
+		bytes = (bits + 7) / 8;
+		map = (unsigned long *)malloc(bytes);
+		if (map == NULL)
+			prl_err(-1, "ENOMEM");
+		return map;
+	}
+	void release()
+	{
+		bsize = 0;
+		gran = 0;
+		free(map);
+		map = NULL;
+	}
+	int save(const std::string &fname) const
+	{
+		int rc = 0, fd;
+		fd = open(fname.c_str(), O_WRONLY|O_CREAT|O_TRUNC);
+		if (fd == -1) {
+			prl_err(-1, "Cannot open %s: %m", fname.c_str());
+			return -1;
+		}
+		prl_log(0, "\tsave CBT %s size %lu", fname.c_str(), bytes);
+		ssize_t n = TEMP_FAILURE_RETRY(write(fd, map, bytes));
+		if (n == -1) {
+			prl_err(-1, "Cannot write %s: %m", fname.c_str());
+			rc = -1;
+		} else if (n != (ssize_t)bytes) {
+			prl_err(-1, "Write %s truncated", fname.c_str());
+			rc = -1;
+		}
+
+		close(fd);
+		return rc;
+	}
+
+	unsigned long bsize;	/* bitmap block size */
+	unsigned int gran;	/* block size */
+	unsigned long bits;	/* size of bitmap in bits */
+	unsigned long bytes;
+	bmap_t *map;
+};
+
+static unsigned long get_disk_size(PRL_HANDLE hDisk)
+{
+	PRL_RESULT e;
+	void *buf;
+	unsigned int len = 0;
+	unsigned long ret;
+
+	e = PrlDisk_GetDiskInfo(hDisk, NULL, &len);
+	if (PRL_FAILED(e)) {
+		prl_log(e, "%s: failed to GetDiskInfo capacity: %s",
+				__FUNCTION__, get_error_str(e).c_str());
+		return 0;
+	}
+	buf = (void*)malloc(len);
+	e = PrlDisk_GetDiskInfo(hDisk, (PRL_DISK_PARAMETERS *)buf, &len);
+	if (PRL_FAILED(e)) {
+		prl_log(e, "%s: failed to GetDiskInfo: %s",
+				__FUNCTION__, get_error_str(e).c_str());
+		free(buf);
+		return 0;
+	}
+	ret = ((PRL_DISK_PARAMETERS *)buf)->m_SizeInSectors * 512;
+	free(buf);
+	return ret;
+}
+
+static cbt_bitmap *get_cbt_bitmap(PRL_HANDLE hDisk, const char *uuid) 
+{
+	PrlHandle hMap;
+	PRL_RESULT e;
+	unsigned int gran, bmap_size;
+	unsigned long size;
+
+	size = get_disk_size(hDisk);
+	if (size == 0)
+		return NULL;
+
+	prl_log(1, "\t get CBT by uuid %s", uuid);
+	e = PrlDisk_GetChangesMap_Local(hDisk, uuid, NULL, hMap.get_ptr());
+	if (PRL_FAILED(e)) {
+		prl_log(e, "%s: failed to GetChangesMap_Local for uuid %s: %s",
+				__FUNCTION__, uuid, get_error_str(e).c_str());
+		return NULL;
+	}
+	e = PrlDiskMap_GetGranularity(hMap, &gran);
+	if (PRL_FAILED(e)) {
+		prl_log(e, "%s: failed to GetGranularity: %s",
+				__FUNCTION__, get_error_str(e).c_str());
+		return NULL;
+	}
+
+	cbt_bitmap *bmap = new cbt_bitmap(size, gran);
+	if (bmap->alloc() == NULL)
+		return NULL;
+	bmap_size = bmap->bytes;
+	e = PrlDiskMap_Read(hMap, (void *)bmap->map, &bmap_size);
+	if (PRL_FAILED(e)) {
+		prl_log(e, "%s: failed to read CBT bitmap: %s",
+				__FUNCTION__, get_error_str(e).c_str());
+		delete bmap;
+		return NULL;
+	}
+	//BUG_ON(bmap_size != ((bmap->size + 7) / 8));
+
+	return bmap;
+}
+
+// Get a bit of a bitmap
+static __inline int BMAP_GET(void const* bmap, unsigned int bit)
+{
+	return !!(((unsigned int const*)bmap)[bit >> 5] & (1 << (bit & 31)));
+}
+
+static int is_zero_block(void *buf, unsigned long size)
+{
+	return *(unsigned long *)buf == 0 &&
+		!memcmp(buf, (char *)buf + sizeof(unsigned long), size - sizeof(unsigned long));
+}
+
+static int store(PRL_HANDLE hDisk, int id, const cbt_bitmap *bmap, const std::string &dir)
+{
+	int ff, fd, rc;
+	void *buf;
+	std::stringstream s;
+
+	s << dir << "/" << id;
+
+	std::string f = s.str() + ".full";
+	std::string d = s.str() + ".delta";
+	std::string c = s.str() + ".cbt";
+
+	prl_log(0, "process disk %d size: %lu", id, bmap->bsize);
+	rc = bmap->save(c);
+	if (rc)
+		return -1;
+
+
+	prl_log(0, "\tsave backup %s blocks: %lu gran: %lu", f.c_str(),
+			bmap->bits, bmap->gran);
+	ff = open(f.c_str(), O_WRONLY|O_CREAT|O_TRUNC);
+	if (ff == -1) {
+		prl_log(-1, "Cannot open %s", f.c_str());
+		return -1;
+	}
+	fd = open(d.c_str(), O_WRONLY|O_CREAT|O_TRUNC);
+	if (fd == -1) {
+		prl_log(-1, "Cannot open %s", d.c_str());
+		close(ff);
+		return -1;
+	}
+	buf = aligned_alloc(4096, bmap->gran);
+	if (buf == NULL) {
+		close(ff);
+		close(fd);
+		return -1;
+	}
+
+	// Make the same size
+	bzero(buf, bmap->gran);
+	TEMP_FAILURE_RETRY(pwrite(ff, buf, bmap->gran, bmap->bits * bmap->gran - bmap->gran));
+	TEMP_FAILURE_RETRY(pwrite(fd, buf, bmap->gran, bmap->bits * bmap->gran - bmap->gran));
+	for (unsigned long n = 0; n < bmap->bits; n++) {
+		rc = PrlDisk_Read(hDisk, buf, bmap->gran, n * bmap->gran / 512);
+		if (rc) {
+			prl_err(rc, "PrlVmBackup_GetDisk: %s",
+					get_error_str(rc).c_str());
+			break;
+		}
+		if (BMAP_GET(bmap->map, n)) {
+			//prl_log(0, "Bit %d", n);
+			ssize_t w = TEMP_FAILURE_RETRY(pwrite(fd, buf, bmap->gran, n * bmap->gran));
+			if (w != bmap->gran) {
+				prl_err(-1, "pwrite %s: %m", d.c_str());
+				rc = -1;
+				break;
+			}
+		}
+
+		if (is_zero_block(buf, bmap->gran))
+			continue;
+
+//		prl_log(0, "Block %d", n);
+		ssize_t w = TEMP_FAILURE_RETRY(pwrite(ff, buf, bmap->gran, n * bmap->gran));
+		if (w != bmap->gran) {
+			prl_err(-1, "pwrite %s: %m", f.c_str());
+			rc = -1;
+			break;
+		}
+	}
+	free(buf);
+	close(ff);
+	close(fd);
+
+	return rc;
+}
+
 /* Example of using PrlVm_BeginBackup()/PrlVmBackup_Commit() */
-int PrlSrv::do_vm_abackup(const PrlVm& vm)
+int PrlSrv::do_vm_abackup(const PrlVm& vm, const BackupParam& param)
 {	
 	PRL_RESULT ret, r;
 	PRL_UINT32 n;
 	std::string err;
 	PrlHandle hResult;
 
-	PrlHandle j(PrlVm_BeginBackup(vm.get_handle(), 0));
+	PrlHandle j(PrlVm_BeginBackup(vm.get_handle(), PBMBF_CREATE_MAP));
 	if ((ret = get_job_result(j, hResult.get_ptr(), &n))) {
 		prl_err(ret, "Failed to begin backup %s",
 				 err.c_str());
@@ -239,29 +458,29 @@ int PrlSrv::do_vm_abackup(const PrlVm& vm)
 	}
 
 	prl_log(0, "Disk count %d", n);
-
-	void *buf = aligned_alloc(4096, 4096);
 	for (unsigned int i = 0; i < n; i++) {
+		int rc;
 		PrlHandle hDisk;
-		ret = PrlVmBackup_GetDisk(hBackup, i, hDisk.get_ptr());
-		if (ret) {
+		
+		ret = -1;
+		rc = PrlVmBackup_GetDisk(hBackup, i, hDisk.get_ptr());
+		if (rc) {
 			prl_err(ret, "PrlVmBackup_GetDisk: %s",
-					get_error_str(ret).c_str());
+					get_error_str(rc).c_str());
 			break;
 		}
 
-		for (i = 0; i < 100; i++) {
-			ret = PrlDisk_Read(hDisk, buf, 4096, 4096/512 * i );
-			if (ret) {
-				prl_err(ret, "PrlVmBackup_GetDisk: %s",
-					get_error_str(ret).c_str());
-				break;
-			}
-		}
+		std::auto_ptr<cbt_bitmap> bmap(get_cbt_bitmap(hDisk.get_handle(),
+					param.flags == PBT_FULL ? NULL : param.uuid.c_str()));
+		if (bmap.get() == NULL)
+			break;
+
+		rc = store(hDisk, i, bmap.get(), param.path);
+		if (rc)
+			break;
+		ret = 0;
 	}
 
-	free(buf);
-	
 	PrlHandle c(ret ? PrlVmBackup_Rollback(hBackup) : PrlVmBackup_Commit(hBackup));
 	if ((r = get_job_retcode(c, err))) {
 		prl_err(ret, "Failed to backup %s %s: %s",
@@ -269,6 +488,22 @@ int PrlSrv::do_vm_abackup(const PrlVm& vm)
 				vm.get_vm_type_str(), err.c_str());
 	}
 
+	if (ret == 0) {
+		char uuid[64] = "";
+		unsigned int len = sizeof(uuid);
+		ret = PrlVmBackup_GetUuid(hBackup, uuid, &len);
+		if (ret) {
+			prl_err(ret, "Failed to get backup uuid: %s",
+					get_error_str(ret).c_str());
+			return ret;
+		}
+
+		std::string f = param.path + "/uuid";
+		std::ofstream out(f.c_str());
+		out << uuid;
+		out.close();
+		prl_log(0, "Backup created uuid: %s", uuid);
+	}
 	return ret;
 }
 
@@ -291,6 +526,8 @@ int PrlSrv::backup_vm(const CmdParamData& param)
 			(ret = storage.login(param.backup.storage)))
 		return ret;
 
+	if (param.backup.abackup)
+		return do_vm_abackup(*vm, param.backup);
 	return do_vm_backup(*vm, param, storage);
 }
 
